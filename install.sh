67287544f22a120e0dd8c22d67c503ecd0710743
Ty <809015137@qq.com>
2025-10-19 00:28:24 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,3 @@
+mkdir /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/bits
+curl https://raw.githubusercontent.com/ty-yqs/macOS-stdc.h/refs/heads/main/bits/stdc%2B%2B.h > /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/bits/stdc++.h
+echo "Fine!"
